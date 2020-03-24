d99668bbc5f03014628c7f2f21e4ee4f2fb9cccf
owen <844027443@qq.com>
2020-03-25 00:01:06 +0800

    suwq_2020 完成登录界面

--- src/router.js
+++ src/router.js
@@ -1,42 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
-import Home from './components/Home.vue'
-import Welcome from './components/Welcome.vue'
-import User from './components/user/User.vue'
-import Rights from './components/right/Rights.vue'
-import Roles from './components/right/Roles.vue'
-import Cate from './components/goods/Categories.vue'
-import Params from './components/goods/Params.vue'
 
 Vue.use(Router)
 
 const router =  new Router({
   routes: [
     {path:'/',redirect:'/login'},
-    {path:'/login',component:Login},
-    {path:'/home',redirect:'/welcome',component:Home,children:[
-      {path:'/welcome',component:Welcome},
-      {path:'/users',component:User},
-      {path:'/rights',component:Rights},
-      {path:'/roles',component:Roles},
-      {path:'/categories',component:Cate},
-      {path:'/params',component:Params}
-    ]},
+    {path:'/login',component:Login}
   ]
-})
-
-router.beforeEach((to,from,next)=>{
-  // console.log(to)
-  if(to.path=='/login'){
-    next()
-  }else{
-    if(sessionStorage.getItem('token')){
-      next()
-    }else {
-      next('/login')
-    }
-  }
 })
 
 export default router
